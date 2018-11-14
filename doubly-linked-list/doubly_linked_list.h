d19dfbe55d0c9431fb8bb38666a07c5e56741551
lebmunoz <le.bmunoz@gmail.com>
2018-11-14 13:27:26 -0200

    docs pronto code wip

--- doubly-linked-list/doubly_linked_list.h
+++ doubly-linked-list/doubly_linked_list.h
@@ -1,74 +1,80 @@
+// Copyright 2018 Luis Enrique Munoz
 namespace structures {
 
 template<typename T>
+    //! Classe Lista Duplamente Encadeada
 class DoublyLinkedList {
-public:
-    DoublyLinkedList()
-    ~DoublyLinkedList()
-    void clear()
+ public:
+    //! Construtor padrão
+    DoublyLinkedList();
+    //! Destrutor
+    ~DoublyLinkedList();
+    //! Método limpar
+    void clear();
 
     //! insere no fim
-    void push_back(const T& data)
+    void push_back(const T& data);
     //! insere no início
-    void push_front(const T& data)
+    void push_front(const T& data);
     //! insere na posição
-    void insert(const T& data, std::size_t index)
+    void insert(const T& data, std::size_t index);
     //! insere em ordem
-    void insert_sorted(const T& data)
+    void insert_sorted(const T& data);
 
     //! retira da posição
-    T pop(std::size_t index)
+    T pop(std::size_t index);
     //! retira do fim
-    T pop_back()
+    T pop_back();
     //! retira do início
-    T pop_front()
+    T pop_front();
     //! retira específico
-    void remove(const T& data)
+    void remove(const T& data);
 
     //! lista vazia
-    bool empty() const
+    bool empty(); const
     //! contém
-    bool contains(const T& data) const
+    bool contains(const T& data); const
 
-    //! acesso a um elemento (checando limites)
-    T& at(std::size_t index)
+    //! acesso a um elemento (checando limites);
+    T& at(std::size_t index);
     //! getter constante a um elemento
-    const T& at(std::size_t index) const
+    const T& at(std::size_t index); const
 
     //! posição de um dado
-    std::size_t find(const T& data) const
+    std::size_t find(const T& data); const
     //! tamanho
-    std::size_t size() const
-private:
+    std::size_t size(); const
+
+ private:
     class Node {
-    public:
-        Node(const T& data)
-        Node(const T& data, Node* next)
-        Node(const T& data, Node* prev, Node* next)
+     public:
+        explicit Node(const T& data);
+        Node(const T& data, Node* next);
+        Node(const T& data, Node* prev, Node* next);
 
-        T& data()
-        const T& data() const
+        T& data();
+        const T& data(); const
 
-        Node* prev()
-        const Node* prev() const
+        Node* prev();
+        const Node* prev(); const
 
-        void prev(Node* node)
+        void prev(Node* node);
 
-        Node* next()
-        const Node* next() const
+        Node* next();
+        const Node* next(); const
 
-        void next(Node* node)
-    private:
+        void next(Node* node);
+     private:
         T data_;
         Node* prev_;
         Node* next_;
     };
 
     //! primeiro da lista
-    Node* head; 
+    Node* head;
     //! ultimo da lista
-    Node* tail; 
+    Node* tail;
     std::size_t size_;
 };
 
-}
+}  // namespace structures
