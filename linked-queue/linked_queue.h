b0207e4e7fd29e30177f3d5acd465d4b150b53fc
lebmunoz <le.bmunoz@gmail.com>
2018-11-14 11:49:41 -0200

    resolução final lista encadeada

--- linked-queue/linked_queue.h
+++ linked-queue/linked_queue.h
@@ -40,6 +40,53 @@ class LinkedQueue {
     std::size_t size() const;
 
  private:
+    //! Elemento
+    class Node {
+     public:
+        explicit Node(const T& data):
+            data_{data}
+        {}
+
+        Node(const T& data, Node* next):
+            data_{data},
+            next_{next}
+        {}
+
+        //! getter: dado
+        T& data() {
+            return data_;
+        }
+
+        //! getter const: dado
+        const T& data() const {
+            return data_;
+        }
+
+        //! getter: próximo
+        Node* next() {
+            return next_;
+        }
+
+        //! getter const: próximo
+        const Node* next() const {
+            return next_;
+        }
+
+        //! setter: próximo
+        void next(Node* node) {
+            next_ = node;
+        }
+
+        //! setter: dado
+        void data(T data) {
+            data_ = data;
+        }
+
+     private:
+        T data_;
+        Node* next_{nullptr};
+    };
+
     //! nodo-cabeça
     Node* head;
     //! nodo-fim
@@ -48,7 +95,7 @@ class LinkedQueue {
     std::size_t size_;
 };
 
-}  // Namespace structures
+}  // namespace structures
 
 #endif
 
@@ -73,50 +120,62 @@ void structures::LinkedQueue<T>::clear() {
 
 template<typename T>
 void structures::LinkedQueue<T>::enqueue(const T& data) {
+    Node* novo = new Node(data, NULL);
     if (empty()) {
-        Node* novo = new Node(data, head);
-        if (novo == NULL) {
-            throw std::out_of_range("Sem memória!");
-        } else {
-            novo->next(head);
-            head = novo;
-            size_++;
-        }
+        head = novo;
     } else {
-        Node* anterior = head;
-        for (size_t i = 1u; i < index; i++) {
-            anterior = anterior->next();
-        }
-        Node* novo = new Node(data, anterior->next());
-        if (novo == NULL) {
-            throw std::out_of_range("Memória cheia");
-        }
-        anterior->next(novo);
-        size_++;
+        tail->next(novo);
     }
-}
+    novo->next(NULL);
+    novo->data(data);
+    tail = novo;
+    size_++;
+    }
 
 template<typename T>
 T structures::LinkedQueue<T>::dequeue() {
-
+    Node* deleta;
+    T retorno;
+    if (empty()) {
+        throw std::out_of_range("Fila vazia!");
+    } else {
+        deleta = head;
+        retorno = deleta->data();
+        head = deleta->next();
+        if (size_ == 1) {
+            tail = NULL;
+        }
+        size_--;
+        delete deleta;
+    }
+    return retorno;
 }
 
 template<typename T>
 T& structures::LinkedQueue<T>::front() const {
-
+    if (size() == 0) {
+        throw std::out_of_range("Fila vazia");
+    } else {
+        return head->data();
+    }
 }
+
 
 template<typename T>
 T& structures::LinkedQueue<T>::back() const {
-
+    if (size() == 0) {
+        throw std::out_of_range("Fila vazia");
+    } else {
+        return tail->data();
+    }
 }
 
 template<typename T>
 bool structures::LinkedQueue<T>::empty() const {
-
+    return size_ == 0;
 }
 
 template<typename T>
 std::size_t structures::LinkedQueue<T>::size() const {
-
+    return size_;
 }
