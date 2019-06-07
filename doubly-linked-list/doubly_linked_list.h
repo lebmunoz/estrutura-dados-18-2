b084535c93c6f54fab9becfb56f704f5c710d959
lebmunoz <le.bmunoz@gmail.com>
2019-06-07 19:54:09 -0300

    implementação lista duplamente encadeada wip

--- doubly-linked-list/doubly_linked_list.h
+++ doubly-linked-list/doubly_linked_list.h
@@ -56,18 +56,36 @@ class DoublyLinkedList {
         Node(const T& data, Node* next);
         Node(const T& data, Node* prev, Node* next);
 
-        T& data();
-        const T& data() const;
+        T& data() {
+            return data_;
+        }
 
-        Node* prev();
-        const Node* prev() const;
+        const T& data() const {
+            return data_;
+        }
 
-        void prev(Node* node);
+        Node* prev() {
+            return prev_;
+        }
 
-        Node* next();
-        const Node* next() const;
+        const Node* prev() const {
+            return prev_;
+        }
 
-        void next(Node* node);
+        void prev(Node* node) {
+            prev_ = node;
+        }
+
+        Node* next() {
+            return next_;
+        }
+        const Node* next() const {
+            return next_;
+        }
+
+        void next(Node* node) {
+            next_ = node;
+        }
      private:
         T data_;
         Node* prev_;
@@ -97,7 +115,7 @@ structures::DoublyLinkedList<T>::~DoublyLinkedList() {
 
 template<typename T>
 void structures::DoublyLinkedList::clear() {
-    while(!empty()) {
+    while (!empty()) {
         pop_front();
     }
 }
@@ -109,6 +127,101 @@ void structures::DoublyLinkedList::push_back(const T& data) {
 
 template<typename T>
 void structures::DoublyLinkedList::push_front(const T& data) {
+    Node* aux = new Node(data);
+    if (aux == NULL) {
+        throw std::out_of_range("Memória cheia!");
+    } else {
+        aux->next(head);
+        aux->prev(NULL);
+        head = aux;
+        if (aux->next() =! NULL) {
+            aux->next->prev(aux);
+        } else {
+            tail = aux;
+        }
+        size_++;
+    }
+}
+
+template<typename T>
+void structures::DoublyLinkedList::insert(const T& data, std::size_t index) {
+    Node* novo = new Node(data);
+    Node* anterior;
+    if (novo == NULL) {
+        throw std::out_of_range("Memória cheia!");
+    } else if (index > size_ || index < 0) {
+        throw std::out_of_range("Posição inválida!");
+    } else if (index == 0) {
+        push_front(data);
+    } else {
+        anterior = head;
+        for (int i = 0; i < index; ++i) {
+            anterior = anterior->next();
+        }
+        novo->next(anterior->next());
+        if (novo->next() != NULL) {
+            novo->next->prev(novo);
+        } else {
+            tail = novo;
+        }
+        novo->next(anterior->next());
+        anterior->next(novo);
+        novo->prev(anterior);
+        size_++;
+    }
+}
+
+template<typename T>
+void structures::DoublyLinkedList::insert_sorted(const T& data) {
     insert(data, 1u);
 }
 
+template<typename T>
+T structures::DoublyLinkedList::pop(std::size_t index) {
+    insert(data, 1u);
+}
+
+template<typename T>
+T structures::DoublyLinkedList::pop_back() {
+    insert(data, 1u);
+}
+
+template<typename T>
+T structures::DoublyLinkedList::pop_front() {
+    insert(data, 1u);
+}
+
+template<typename T>
+void structures::DoublyLinkedList::remove(const T& data) {
+    insert(data, 1u);
+}
+
+template<typename T>
+bool structures::DoublyLinkedList::empty() const {
+    insert(data, 1u);
+}
+
+template<typename T>
+bool structures::DoublyLinkedList::contains(const T& data) const {
+    insert(data, 1u);
+}
+
+template<typename T>
+T& structures::DoublyLinkedList::at(std::size_t index) {
+    insert(data, 1u);
+}
+
+template<typename T>
+const T& structures::DoublyLinkedList::at(std::size_t index) const {
+    insert(data, 1u);
+}
+
+template<typename T>
+std::size_t structures::DoublyLinkedList::find(const T& data) const {
+    insert(data, 1u);
+}
+
+template<typename T>
+std::size_t structures::DoublyLinkedList::size() const {
+    insert(data, 1u);
+}
